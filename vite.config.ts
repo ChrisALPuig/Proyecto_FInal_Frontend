d8dc292c89fa7570511ff1176f14130c8c83b40c
carias <carias@elpuig.xeill.net>
2026-05-17 01:42:20 +0200

    Primer COmmit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
@@ -9,9 +8,13 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    legacy(),
     tailwindcss()
   ],
+
+  build: {
+    target: 'esnext'
+  },
+
   test: {
     globals: true,
     environment: 'jsdom',
